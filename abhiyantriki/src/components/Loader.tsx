795ca730e418c7124b901831954d4293defd35fc
Suja16 <sujal16@somaiya.edu>
2024-10-09 04:53:41 +0530

    hello

---
+++ abhiyantriki/src/components/Loader.tsx
@@ -0,0 +1,13 @@
+import { Component } from "solid-js";
+
+const Loader: Component = () => {
+  return (
+    <img
+      src="  loader.gif"
+      alt="Loading..."
+      style={{ height: "100vh", width: "100vw" }}
+    />
+  );
+};
+
+export default Loader;
